d280eb37d5ac9ebb729474d7a8a48f94367a106d
Solsvik, Jonas <fmfsjoso@fylkesmannen.no>
2017-06-16 10:05:36 +0200

    new App.js

--- OrdBase/App_Start/BundleConfig.cs
+++ OrdBase/App_Start/BundleConfig.cs
@@ -8,22 +8,32 @@ namespace OrdBase
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle(
+                "~/bundles/jquery").Include(
+                    "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle(
+                "~/bundles/modernizr").Include(
+                    "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle(
+                "~/bundles/bootstrap").Include(
+                    "~/Scripts/bootstrap.js",
+                    "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/App.css"));
+
+            // Javascript code for App.html
+            bundles.Add(new ScriptBundle(
+                "~/bundles/App").Include(
+                    "~/Scripts/App.js"));
+
+            bundles.Add(new StyleBundle(
+                "~/Content/css").Include(
+                    "~/Content/bootstrap.css",
+                    "~/Content/site.css",
+                    "~/Content/App.css"));
         }
     }
 }
---
+++ OrdBase/Scripts/App.js
@@ -0,0 +1,7 @@
+//
+// @file App.js
+// @creator Jonas Solsvik
+// @created 16.06.2017
+// @brief User defined Javascript code for App.cshtml
+//
+
