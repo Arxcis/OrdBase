36642f1d2d670162a5ddd6ff5bbf595860760b7f
Solsvik, Jonas <fmfsjoso@fylkesmannen.no>
2017-06-21 09:41:40 +0200

    Backend: Added repository-services

--- OrdBase/Controllers/AppController.cs
+++ OrdBase/Controllers/AppController.cs
@@ -10,12 +10,6 @@ namespace OrdBase.Controllers
 {
     public class AppController : Controller
     {
-        private TranslationDb db = new TranslationDb();
-
-        private List<string> GetClientCategories(string client)
-        {
-            return new List<string>{};
-        }
 
         public ActionResult Index(string client = "FMSF")
         {
@@ -25,24 +19,6 @@ namespace OrdBase.Controllers
                 AccessLevel = "Admin",
                 ClientName = client
             });
-        }
-
-
-        public ActionResult Editor(string client, string accessKey)
-        {
-            return PartialView(new EditorViewModel {});
-        }
-
-
-        public ActionResult Selector(string client)
-        {
-            //
-            // @doc Group by - https://stackoverflow.com/questions/7325278/group-by-in-linq
-            // @doc More group by - https://stackoverflow.com/questions/2697253/using-linq-to-group-a-list-of-objects-into-a-new-grouped-list-of-list-of-objects
-            // @doc MSDN Linq examples - https://msdn.microsoft.com/en-us/library/gg509017.aspx
-            //
-            return PartialView(new SelectorViewModel{});
-            
         }
     }
 }
---
+++ OrdBase/Services/TranslationSetRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrdBase.Services
+{
+    public class TranslationSetRepository : IDataStore
+    {
+
+    	   	//
+            // @doc Group by - https://stackoverflow.com/questions/7325278/group-by-in-linq
+            // @doc More group by - https://stackoverflow.com/questions/2697253/using-linq-to-group-a-list-of-objects-into-a-new-grouped-list-of-list-of-objects
+            // @doc MSDN Linq examples - https://msdn.microsoft.com/en-us/library/gg509017.aspx
+            //
+    }
+}
