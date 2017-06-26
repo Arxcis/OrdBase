210601879e1f039e025ef7d979eb4aedd5677ec7
Solsvik, Jonas <fmfsjoso@fylkesmannen.no>
2017-06-26 17:57:40 +0200

    front-end - made first polymer component. It works

--- wwwroot/OrdBasePlugin/api.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-import translation from 'translation';
-import language from 'language';
-import client from 'client';
-import container from 'container';
-
-export default { translation, language, client, container }
