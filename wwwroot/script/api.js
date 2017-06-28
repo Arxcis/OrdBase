a4a8a039d0aae6092ae3812e71cc7336433138a8
Solsvik, Jonas <fmfsjoso@fylkesmannen.no>
2017-06-28 13:54:30 +0200

    Created custom elements, one for each view

--- wwwroot/script/api.js
+++ wwwroot/script/api.js
@@ -1,5 +1,4 @@
-
-'use strict'
+'use strict';
 
 let api = (function(){
 
---
+++ wwwroot/script/controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let controller = (function (){
+
+    let controller = {};
+
+    controller.clientSelector      = function() {};
+    controller.translationEditor   = function(client) {};
+    controller.translationSelector = function(client, accessKey) {};
+});
