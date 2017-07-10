06219ca356523b9d5a45b40593c6a4aad1e709a7
Solsvik, Jonas <fmfsjoso@fylkesmannen.no>
2017-07-10 13:12:14 +0200

    select client looks clearn

--- wwwroot/app/event/OnLoadView/loadSelectClient.js
+++ wwwroot/app/event/OnLoadView/loadSelectClient.js
@@ -4,16 +4,17 @@ import * as api from '../../library/api.js';
 import { loadSelectTranslation } from './loadSelectTranslation.js';
 import { loadEditClient }        from './loadEditClient.js';
 
+// Cache static element references
+const main         = document.body.getElementById('ordbase-main');    
+const header       = document.body.getElementById('ordbase-header');
+
 //
 // @function OnloadViewClientSelector
 //
 export function loadSelectClient() {
 
-    // Do DOM stuff
-    const main   = document.getElementById('ordbase-main');    
-    const header = document.getElementById('ordbase-header');
-    const selectClientElement = document.createElement('ordbase-select-client');
-    const cardClientElement   = document.createElement('ordbase-card-client');
+    // Create elements
+    const selectClient = document.createElement('ordbase-select-client');
 
     // Setup header
     header.textBig      = args.textHeaderBig   || 'Ordbase';
@@ -21,33 +22,18 @@ export function loadSelectClient() {
     header.buttonLeft   = ICON_HEADER_SQUARE;
     header.buttonRight1 = ICON_HEADER_NONE; 
     header.buttonRight2 = ICON_HEADER_PLUS;
-    header.render();
 
-    // Setup select client view
-    
+    // Batch-update DOM
+    main.innerHTML = ''; 
+    header.DOMUpdate();
+    main.appendChild(selectClient); 
 
-    // Get Client data
-    api.client.getAll().then(clientCollection => {
-
-        // Loop through all clients and generate cards for each of thems
-
-        clientCollection.forEach((client, index) => {
-    
-            const cardContent = unpackTemplate(cardTemplate, {
-                id:         `card${index}`,
-                name:       client.name,
-                website:    'https://www.placeholder.no',
-                thumbnail : 'http://placehold.it/250x125/FFC107', 
-            });
-
-            cardContent.querySelector('button').addEventListener('click', event => loadSelectTranslation(client.name));            
-            main.appendChild(cardContent);            
-        });      
-    })
-    .catch(reason => console.error('Error:', reason))
-    .then(() => {                                  
-        // Clear all previous content, insert new view
-        document.body.innerHTML = ''; 
-        document.body.appendChild(view);
-    });
+    // @ajax - Fetch client data from server
+    api.client.getAll().then(clientObjects => {                             
+                            // Inject data into view
+                            selectClient.clients = clientObjects;
+                            // Update DOM again
+                            selectClient.DOMUpdate();
+                        })
+                        .catch(reason => console.error('Error:', reason))
 }
