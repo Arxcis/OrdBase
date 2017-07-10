ba6e785465aa0a41762b5a07df59bb9937af3f3f
Solsvik, Jonas <fmfsjoso@fylkesmannen.no>
2017-07-10 15:44:24 +0200

    commiting to push to master branch

--- wwwroot/app/event/OnLoadView/loadSelectClient.js
+++ wwwroot/app/event/OnLoadView/loadSelectClient.js
@@ -1,12 +1,10 @@
 'use strict';
 
 import * as api from '../../library/api.js';
-import { loadSelectTranslation } from './loadSelectTranslation.js';
-import { loadEditClient }        from './loadEditClient.js';
 
 // Cache static element references
-const main         = document.body.getElementById('ordbase-main');    
-const header       = document.body.getElementById('ordbase-header');
+const main         = document.getElementById('ordbase-main');    
+const header       = document.getElementById('ordbase-header');
 
 //
 // @function OnloadViewClientSelector
@@ -17,20 +15,23 @@ export function loadSelectClient() {
     const selectClient = document.createElement('ordbase-select-client');
 
     // Setup header
-    header.textBig      = args.textHeaderBig   || 'Ordbase';
-    header.textSmall    = args.textHeaderSmall || 'Select Client';
-    header.buttonLeft   = ICON_HEADER_SQUARE;
-    header.buttonRight1 = ICON_HEADER_NONE; 
-    header.buttonRight2 = ICON_HEADER_PLUS;
+    header.textBig          = 'Ordbase';
+    header.textSmall        = 'Select Client';
+    header.buttonIconLeft   = ICON_HEADER_SQUARE;
+    header.buttonIconRight1 = '';    
+    header.buttonIconRight2 = ICON_HEADER_PLUS;
 
     // Dependency injection
-    selectClient.buttonHandler = loadSelectTranslation;
+    header.buttonHandlerLeft   = {};
+    header.buttonHandlerRight1 = {};      
+    header.buttonHandlerRight2 = {};   
+    selectClient.buttonHandler = {};
 
     // Batch-update DOM
     main.innerHTML = ''; 
     header.DOMUpdate();
-    main.appendChild(selectClient); 
-
+    main.appendChild(selectClient);              
+    
     // @ajax - Fetch client data from server
     api.client.getAll().then(clientObjects => {             
                 
@@ -38,7 +39,7 @@ export function loadSelectClient() {
                             selectClient.clients = clientObjects; 
                             
                             // Update DOM again
-                            selectClient.DOMUpdate();             
+                            selectClient.DOMUpdate();
                         })
                         .catch(reason => console.error('Error:', reason))
 }
