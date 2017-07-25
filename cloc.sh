fabb5a3b4bf9ac43299e033078cc5fe6bed12f2a
Solsvik, Jonas <fmfsjoso@fylkesmannen.no>
2017-07-25 16:20:53 +0200

    cloc.sh counts lines of code

---
+++ cloc.sh
@@ -0,0 +1,13 @@
+cloc Controllers/ \
+           IData/ \
+           Models/ \
+           Repositories/ \
+           wwwroot/app/ \
+           wwwroot/lib/ \
+           wwwroot/components \
+           wwwroot/css \
+           wwwroot/views \
+           wwwroot/index.html \
+           wwwroot/webpack.config.js \
+           program.cs \
+           Startup.cs
